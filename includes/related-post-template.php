e0ebd0b3d8aea529678fdf97f7bd284ab8c640c8
nelio <nelio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-02 09:02:23 +0000

    Uploading version 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/nelio-related-posts/tags/1.0.2@906769 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/related-post-template.php
@@ -0,0 +1,52 @@
+<?php
+if ( !class_exists( 'NelioSRPRelatedPostTemplate' ) ) {
+class NelioSRPRelatedPostTemplate {
+
+public static function render( $post ) {
+	ob_start();
+
+	$open_link = sprintf(
+		'<a class="related_post_link" href="%s" title="%s">',
+		get_permalink( $post->ID ),
+		esc_attr( apply_filters( 'the_title', $post->post_title ) )
+	);
+	$close_link = '</a>';
+	?>
+
+	<article class="post-<?php echo $post->ID; ?> post type-post status-publish entry" itemscope="itemscope">
+		<div class="entry-title">
+			<p><?php
+				echo $open_link;
+				echo apply_filters( 'the_title', $post->post_title );
+				echo $close_link;
+			?></p>
+		</div>
+		<?php
+		if ( has_post_thumbnail( $post->ID ) ) { ?>
+		<div class="featured-image alignleft">
+			<?php
+			echo $open_link;
+			echo get_the_post_thumbnail( $post->ID, 'thumbnail' );
+			echo $close_link;
+			?>
+		</div>
+		<?php
+		} ?>
+		<div class="entry-content">
+			<?php echo neliosrp_extract_excerpt( $post, NelioSRPSettings::get_max_num_of_words_in_excerpt() ); ?>
+			<?php
+			echo $open_link;
+			echo NelioSRPSettings::get_read_more_label();
+			echo $close_link;
+			?>
+		</div>
+	</article>
+<?php
+
+	$value = ob_get_contents();
+	ob_end_clean();
+	return $value;
+}
+
+} }
+?>
